5265ad4a9ba5d04124ed173fe72cb6d2de241957
Christian Harrison <cjh2565@icloud.com>
2022-03-13 15:46:47 -0400

    Section 2.3 Added message button

--- You are awesome!/ViewController.swift
+++ You are awesome!/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("👍 viewDidLoad has run!")
+        messageLabel.text = "Fabulous?"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ButtonPressed(_ sender: Any) {
+        print("😀 The message button was pressed")
+        messageLabel.text = "You are awesome!"
+        
+    }
+    
 }
 
